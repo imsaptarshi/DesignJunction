7ed97eddaaf0cedeec3526efae0e667d45cdd067
imsaptarshi <123saptarshi.basu@gmail.com>
2021-08-15 18:28:24 +0530

    fixed minor issues

--- helpers/searcher.ts
+++ helpers/searcher.ts
@@ -7,54 +7,31 @@ const includes = (list: Array<String>, query: String) => {
     return false;
 };
 
+const isDocPresent = (list: Array<any>, query: String) => {
+    for (var i = 0; i < list.length; i++) {
+        if (String(list[i]._id) === String(query)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 const SearchResource = (list: any, query: any) => {
     console.log(list)
-    const resourcesWithTitle = []
+    const resources = []
 
     for (var i = 0; i < list.length; i++) {
         if (list[i].title.search(RegExp(String(query), "i")) !== -1 || list[i].description.search(RegExp(String(query), "i")) !== -1) {
-            resourcesWithTitle.push(list[i]);
+            resources.push(list[i]);
         }
     }
-    console.log(resourcesWithTitle);
-    const resourcesWithTag = [];
     for (let i = 0; i < list.length; i++) {
         if (includes(list[i].tags, String(query))) {
-            resourcesWithTag.push(list[i]);
+            if (!isDocPresent(resources, String(list[i]._id)))
+                resources.push(list[i]);
         }
     }
-    const resources = [];
-    for (
-        let i = 0;
-        i < Math.min(resourcesWithTag.length, resourcesWithTitle.length);
-        i++
-    ) {
-        if (
-            String(resourcesWithTag[i]._id) === String(resourcesWithTitle[i]._id)
-        ) {
-            resources.push(resourcesWithTag[i]);
-        } else {
-            resources.push(resourcesWithTag[i]);
-            resources.push(resourcesWithTitle[i]);
-        }
-    }
-    if (resourcesWithTag.length > resourcesWithTitle.length) {
-        for (
-            let i = resourcesWithTitle.length;
-            i < resourcesWithTag.length;
-            i++
-        ) {
-            resources.push(resourcesWithTag[i]);
-        }
-    } else {
-        for (
-            let i = resourcesWithTag.length;
-            i < resourcesWithTitle.length;
-            i++
-        ) {
-            resources.push(resourcesWithTitle[i]);
-        }
-    }
+
     return resources;
 }
 
